2032bc4e4ab145e289fb03442c3e811775fb61e0
Chua Tse Hui <106983097+cth06-Github@users.noreply.github.com>
2025-09-28 16:04:30 +0800

    Add comment to indicate where to edit the functionality

--- components/scroll-to-top/scroll-to-top.js
+++ components/scroll-to-top/scroll-to-top.js
@@ -1,6 +1,6 @@
 function scrollToTop() {
   window.scrollTo({
-    top: 0,
+    top: 0, // Scroll to the top of the page. Edit the number here to scroll to a specific pixel value
     behavior: "smooth",
   });
 }
